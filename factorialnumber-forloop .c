31e629f316f538464a82e9cd5e9221e3eda10fc6
mayankbutanii22 <162984156+mayankbutanii22@users.noreply.github.com>
2024-04-29 21:26:29 +0530

    Add files via upload

---
+++ factorialnumber-forloop .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a,N;
+	int sum=1;
+	printf("Enter any number: ");
+	scanf("%d",&N);
+	
+	for(a=1; a<=5; a++){
+		
+		sum=sum*a;
+			
+	}
+	printf("The factorial is: %d",sum);
+}
