66a6173d12c9d0bbb8b33de86941a64e62ff99d4
mayankbutanii22 <162984156+mayankbutanii22@users.noreply.github.com>
2024-04-29 20:53:38 +0530

    Add files via upload

---
+++ multiplicationtable-forloop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a,N;
+	printf("Enter any number: ");
+	scanf("%d",&N);
+	
+	
+  for (int a=1; a<=10; a++) {
+  	
+  	 printf("%d * %d = %d \n", N,a, N*a);
+  } 
+ 
+}
